fe614f28859e1f62b6d39fed0a6254b8ebe29900
Fernando <ferbrizie@gmail.com>
2026-03-18 17:11:28 +0100

    started parsing of datacsv and still hace to do with bitcin price

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,6 +2,26 @@
 #include <string>
 #include <fstream>
 
+
+bool	isLeapYear(int year)
+{
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+bool	isValidDate(int year, int month, int day)
+{
+	static const int daysInMonths[12] = {31, 28, 31, 30, 31,30, 31, 31, 30, 31,30,31};
+	int maxDays = daysInMonths[month - 1];
+	if (day < 1 || day > maxDays || month > 12 || month < 1 )
+	{
+		return false;
+	}
+	if (month == 2 && isLeapYear(year))
+	{
+		maxDays = 29;
+	}
+	return day <=maxDays;
+}
 bool	parseDate(const std::string & date)
 {
 	//std::cout<<trimmedate<<std::endl;
@@ -31,35 +51,38 @@ bool	parseDate(const std::string & date)
 	(date[start + 6] - '0');
 	int day = (date[start + 8] - '0') * 10 +
 	(date[start + 9] - '0');
-	std::cout<<month<<" "<<day<<" "<<year<<std::endl;
-	std::cout<<date<<std::endl;
-	if (month < 1 || month > 12)
-		return false;
-	static const int daysInMonths[12] = {31, 28, 31, 30, 31,30, 31, 31, 30, 31,30,31};
-	bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-	int maxDays = daysInMonths[month - 1];
-	if (month == 2 && isLeapYear)
-	{
-		maxDays = 29;
-	}
-	if (day < 1 || day > maxDays)
-	{
-		return false;
-	}
-	return true;
+	//std::cout<<month<<" "<<day<<" "<<year<<std::endl;
+	//std::cout<<date<<std::endl;
+	return isValidDate(year, month, day);
 }
 
+bool	parsePrice(const std::string &price)
+{
+	std::map<std::string,  int> database;
+//	size_t start = price.find_first_not_of(" \t");
+//	size_t end = price.find_last_not_of(" \t");
+	float result;
+	std::stringstream ss(price)
+	std::cout<<result<<std::endl;
+	if(!(ss >> result)|| result < 0)
+		std::cout<<"Error: invalid price"<<std::endl;
+}
 
-
-bool	parseInput(const std::string &line)
+bool	parseInput(const std::string &line, char divider)
 {
 	size_t pos;
 	std::string	date;
+	std::string	price;
 	//std::cout<<line<<std::endl;
-	pos = line.find("|");
+	pos = line.find(divider);
 	if(pos == std::string::npos)
 		return false;
 	date = line.substr(0, pos);
+	//pos = line.find("|", pos + 1);
+	price = line.substr(pos + 1 , '\n');
+	//if(pos != std::string::npos)
+	//	return false;
+	std::cout<<price<<std::endl;
 	if(!parseDate(date))
 		return false;
 	return true;
@@ -85,7 +108,7 @@ int main(int argc, char **argv)
 		}
 		while(std::getline(infile, line))
 		{
-			if(!parseInput(line))
+			if(!parseInput(line, '|'))
 			{
 				std::cout<<"Error: Not the right input";
 				return 1;
@@ -97,5 +120,28 @@ int main(int argc, char **argv)
 	{
 		std::cout<<"Something went wrong"<<std::endl;
 	}
-
+	std::ifstream data("data.csv");
+	std::string dataline;
+	if(infile.is_open())
+	{
+		std::getline(data, dataline);
+		if(dataline  != "date,exchange_rate")
+		{
+			std::cout<<"Error: header part of the data file"<<std::endl;
+			return 1;
+			while(std::getline(data, dataline))
+			{
+				if(!parseInput(dataline, ','))
+				{
+					std::cout<<"Error, not the right data input";
+					return 1;
+				}
+			}
+		}
+	}
+	else
+	{
+		std::cout<<"Something went wrong in the datafile"<<std::endl;
+		return 1;
+	}
 }
